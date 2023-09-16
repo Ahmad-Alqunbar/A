1a130bc579df2baf928c080ba6d1875ad8da0401
Ahmad Ali <88638082+ahmadali997@users.noreply.github.com>
2023-09-16 16:50:15 +0300

    register-login-functions

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummy_api;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +31,12 @@ Route::Put('updateStudent/',[StudentController::class,'updateStudent']);
 Route::get('searchStudent/{name?}',[StudentController::class,'searchStudent']);
 
 Route::delete('deleteStudent/{id}',[StudentController::class,'deleteStudent']);
+////////////////////////auth ///////////////////
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
+});
